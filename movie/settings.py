4aa30043729965593bb8e41f2a5388c06d9d4827
prasenjit <99734675+E-15EFT@users.noreply.github.com>
2024-03-04 22:22:44 +0530

    add allow host and add postgress database

--- movie/settings.py
+++ movie/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-&8%*nlx9d!urrpo+3$jxuo-k*k0$$j89rl(2deg*5ektta2c4*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '54.253.228.170']
 
 
 # Application definition
@@ -78,10 +78,23 @@ WSGI_APPLICATION = "movie.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# local database
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
+
+# production database
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'cooking',
+        'USER': 'cooking_delight',
+        'PASSWORD': 'cookingpass',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
@@ -136,12 +149,12 @@ LOGIN_URL = "admin/login"
 
 # DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-# AWS_ACCESS_KEY_ID = 'AKIAZX3C4ENPTWUG5WFW'
-# AWS_SECRET_ACCESS_KEY = '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'
-# AWS_STORAGE_BUCKET_NAME = 'cookingdelight'
-# AWS_S3_SIGNATURE_NAME = 's3v4',
-# AWS_S3_REGION_NAME = 'us-east-1'
-# AWS_S3_FILE_OVERWRITE = False
-# AWS_DEFAULT_ACL =  None
-# AWS_S3_VERITY = True
-# DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+AWS_ACCESS_KEY_ID = 'AKIAZX3C4ENPTWUG5WFW'
+AWS_SECRET_ACCESS_KEY = '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'
+AWS_STORAGE_BUCKET_NAME = 'cookingdelight'
+AWS_S3_SIGNATURE_NAME = 's3v4',
+AWS_S3_REGION_NAME = 'us-east-1'
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL =  None
+AWS_S3_VERITY = True
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
